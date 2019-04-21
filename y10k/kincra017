0bc0e51cc614f521e4e2fdf532063ce400bb6696
unknown <waitesid@gmail.com>
2019-04-21 15:44:57 -0400

    easter commit to upload y10k

---
+++ y10k/kincra017
@@ -0,0 +1,77 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. RRI803.
+       AUTHOR. SID WAITE.
+       ENVIRONMENT DIVISION.
+       INPUT-OUTPUT SECTION.
+       FILE-CONTROL.
+           SELECT CHARCUR ASSIGN TO "S30" ORGANIZATION IS INDEXED
+           ACCESS IS DYNAMIC RECORD KEY IS CHARCUR-KEY
+           ALTERNATE RECORD KEY IS CC-PAYCODE WITH DUPLICATES
+           LOCK MODE MANUAL.
+           SELECT FILEOUT ASSIGN TO "S35" ORGANIZATION 
+           LINE SEQUENTIAL.
+       DATA DIVISION.
+       FILE SECTION.
+       FD FILEOUT
+           DATA RECORD IS FILEOUT01.
+       01  FILEOUT01 PIC X(156).
+       FD  CHARCUR
+           BLOCK CONTAINS 5 RECORDS
+           DATA RECORD IS CHARCUR01.
+       01  CHARCUR01.
+           02 CHARCUR-KEY.
+             03 CC-KEY8 PIC X(8).
+             03 CC-KEY3 PIC XXX.
+           02 CC-PATID PIC X(8).
+           02 CC-CLAIM PIC X(6).
+           02 CC-SERVICE PIC X.
+           02 CC-DIAG PIC X(7).
+           02 CC-PROC PIC X(7).
+           02 CC-MOD2 PIC XX.
+           02 CC-MOD3 PIC XX.
+           02 CC-MOD4 PIC XX.
+           02 CC-AMOUNT PIC S9(4)V99.
+           02 CC-DOCR PIC X(3).
+           02 CC-DOCP PIC 9(2).
+           02 CC-PAYCODE PIC 999.
+           02 CC-STUD PIC X.
+           02 CC-WORK PIC XX.
+           02 CC-DAT1 PIC X(8).
+           02 CC-RESULT PIC X.
+           02 CC-ACT PIC X.
+           02 CC-SORCREF PIC X.
+           02 CC-COLLT PIC X.
+           02 CC-AGE PIC X.
+           02 CC-PAPER PIC X.
+           02 CC-PLACE PIC X.
+           02 CC-IOPAT PIC X.
+           02 CC-DATE-T PIC X(8).
+           02 CC-DATE-A PIC X(8).
+           02 CC-DATE-P PIC X(8).
+           02 CC-REC-STAT PIC X.
+           02 CC-DX2 PIC X(7).
+           02 CC-DX3 PIC X(7).
+           02 CC-ACC-TYPE PIC X.
+           02 CC-DATE-M PIC X(8).
+           02 CC-ASSIGN PIC X.
+           02 CC-NEIC-ASSIGN PIC X.
+           02 CC-DX4 PIC X(7).
+           02 CC-DX5 PIC X(7).
+           02 CC-DX6 PIC X(7).
+           02 CC-FUTURE PIC X(6).
+       WORKING-STORAGE SECTION.
+       01  ALF3 PIC XXX.
+       01  ALF6 PIC X(6).
+       PROCEDURE DIVISION.
+       P0.
+           OPEN OUTPUT FILEOUT INPUT CHARCUR.
+           MOVE "018" TO CC-PAYCODE
+           START CHARCUR KEY NOT < CC-PAYCODE INVALID GO TO P99.
+       P1. READ CHARCUR NEXT AT END GO TO P99.
+           IF CC-PAYCODE NOT = "018" GO TO P99.
+           IF CC-COLLT NOT = "0" GO TO P1.
+           WRITE FILEOUT01 FROM CHARCUR01.
+           GO TO P1.
+       P99.
+           CLOSE FILEOUT CHARCUR.
+           STOP RUN.
---
+++ y10k/mediation-attempt.cbl
@@ -0,0 +1,69 @@
+mediation scenio 
+
+I. arguments to use to continue with the present contract.
+
+1. memorandum of understanding issued to fd1 by fd3.
+   wherein fd3 wants each user to be paying the same rate.
+   .. fd3 users are  paying a much lower rate than fd1 users.   
+
+2. the contract in place is being interpreted incorrectly.
+   .. the contracts says explicitly fd3 pays fd1 the budgeted
+      items at the percentage elicited in the contract.
+      the contract says the fd3 should be charged at a rate
+      per thousand. taking the budgeted amount for 2018
+      and dividing by the gallons metered to fd3 in 2017 that
+      rate is around $3.50 /1,000. 
+      the definition of the word pumped in the contract is
+      being mis-interpreted. it means pumped to fd3; not
+      pumped to our resevoir! to mean otherwise is to negate
+      the concept of equal rates for fd1 and fd3. even that
+      does not guarantee equal rates; only depreciation
+      methology does; or absorbing fd3 into fd1!     
+
+      
+      
+II. arguments for renegotiating a new contract.
+
+1 get sworn testimony that the contract was negotiable from the
+   day it was signed.   
+   
+2 define the present geography of fd3.
+   .. expansion of fd3 puts presure on the fd1 aquafer
+      
+3 selling fd3 water to non-fd3  clients.
+   .. this also puts pressure on the fd1 aquafer
+   
+3 fd1 must have control over items 1. and 2 to be
+   responsible to fd1 clients.
+
+5. redesign the contract based on a depreciation of assets
+   methods rather than the percentage method used in the present
+   contract. simply put,  fd3 would pay fd1 a percentage of the 
+   depreciation of all assets of fd1 used to pumped water to fd3.
+   that percentage would be calculated using only the total metered
+   water and what fd1 actually pumped to fd3. that percentage
+   would be fd3 metered / total metered. as an example, a pipe
+   whose depreciated yearly value is $1,000 and if fd3 used 35% of
+   all metered water, then the pipes cost of use to fd3 would be
+   $350.00. 
+   the reasoning behind depreciation methology is to plan for expected
+   cost of repair/replace when doing budgeting; for which depreciation
+   is the best way to do this. 
+   
+   
+
+
+   
+III. failure to renegoiate a new contract. 
+
+1. plan the sue fd3 for failure to comply to the present
+   contract as described in I.
+
+2. notify fd3 that fd1 no longer wants fd3 to be its client.
+
+
+IV. a letter to fd3 from our attourney that states fd1
+    will not settle for less that the $3.50 / 1,000 rate
+    immediately. and that a renegotiated contract will be
+    including the ideas put forth in II above. 
+    
