5e31afa36e4513f40880268ecb9fbcca76df9df1
stephen waite <cmswest1@gmail.com>
2020-02-25 16:17:15 -0500

    cranes work begins

---
+++ mea358.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stee
+ * Date: 2/25/20
+ * Time: 1:37 PM
+ */
+
+$handle_dat = fopen("mea358", "r");
+//$handle_sid = fopen("w1", "r");
+$handle_ste = fopen("wste", "w");
+$mea = array();
+
+while (($line_dat = fgets($handle_dat)) !== false) {
+    $mea[] = explode(',', $line_dat);
+    //$new_array = array_keys($mea);
+    //var_dump($mea);
+    //var_dump($new_array);
+    //exit;
+    foreach ($mea as $item){
+        foreach ($item as $value) {
+            $value = trim(preg_replace('/[\x00-\x1F\x7F]/u', '', $value));
+            if ($value !== '') {
+                fwrite($handle_ste, $value . "\n");
+            } else {
+                //var_dump($new_array[$item[0]][0]);
+            }        }
+        $value = array();
+    }
+    $mea = array();
+}
+
+
+
