466f1c2f370de45ece36c65981e7fb9721c32063
unknown <waitesid@gmail.com>
2020-06-05 14:18:16 -0400

    fix typos and syntax
    new fd for garfile

--- y10k/g2012.cob
+++ y10k/g2012.cob
@@ -12,16 +12,16 @@
            ACCESS IS DYNAMIC RECORD KEY IS CARE-KEY
            LOCK MODE MANUAL.
            SELECT GARFILE ASSIGN TO "S35" ORGANIZATION IS INDEXED
-           ACCESS MODE IS DYNAMIC  RECORD KEY IS G-GARNO
+           ACCESS MODE IS DYNAMIC RECORD KEY IS G-GARNO
            LOCK MODE MANUAL.
            SELECT FILEOUT ASSIGN TO "S40" ORGANIZATION
            LINE SEQUENTIAL.
        DATA DIVISION.
        FILE SECTION.
-       FD GARFILE.
-      *     BLOCK CONTAINS 3 RECORDS
-           DATA RECORD IS GARFILE01.
-       01 GARFILE01.
+       FD GARFILE
+      *    BLOCK CONTAINS 3 RECORDS
+           DATA RECORD IS G-MASTER.
+       01 G-MASTER.
            02 G-GARNO PIC X(8).
            02 G-GARNAME PIC X(24).
            02 G-BILLADD PIC X(22).
@@ -30,7 +30,10 @@
            02 G-STATE PIC X(2).
            02 G-ZIP PIC X(9).
            02 G-COLLT PIC X.
-           02 G-PHONE PIC X(10).
+           02 G-PHONE. 
+              03 G-PHONE1 PIC XXX.
+              03 G-PHONE2 PIC XXX.
+              03 G-PHONE3 PIC XXXX.
            02 G-SEX PIC X.
            02 G-RELATE PIC X.
            02 G-MSTAT PIC X.
@@ -41,16 +44,17 @@
            02 G-PRINS PIC XXX.
            02 G-PR-ASSIGN PIC X.
            02 G-PR-OFFICE PIC X(4).
-           02 G-PR-GROUP PIC X(12).
-           02 G-PRIPOL PIC X(14).
+           02 G-PR-GROUP PIC X(10).
+           02 G-PRIPOL PIC X(16).
            02 G-PRNAME PIC X(24).
            02 G-PR-RELATE PIC X.
            02 G-SE-MPLR PIC X(4).
            02 G-SEINS PIC XXX.
            02 G-SE-ASSIGN PIC X.
-           02 G-SE-OFFICE PIC X(4).
-           02 G-SE-GROUP PIC X(12).
-           02 G-SECPOL PIC X(14).
+           02 G-TRINSIND PIC X.
+           02 G-TRINS PIC XXX.
+           02 G-SE-GROUP PIC X(10).
+           02 G-SECPOL PIC X(16).
            02 G-SENAME PIC X(24).
            02 G-SE-RELATE PIC X.
            02 G-INSPEND PIC S9(5)V99.
@@ -91,10 +95,10 @@
        01  ANS PIC X.
        PROCEDURE DIVISION.
        0005-START.
-           OPEN INPUT CAREFILE GARFILE PARMFILE.
+           OPEN INPUT CAREFILE GARFILE.
            OPEN OUTPUT FILEOUT.
        P00.
-           READ CAREFILE AT END GO TO P99.
+           READ CAREFILE NEXT AT END GO TO P99.
 
            IF CR-PROC NOT = "G2012"
                GO TO P00
@@ -111,10 +115,11 @@
 
            MOVE SPACE TO FILEOUT01
 
-           STRING G-NAME "," G-PRIPOL "," CR-DATE ","
+           STRING G-GARNAME "," G-PRIPOL "," CR-DATE ","
                CR-ICN "," CR-PROC INTO FILEOUT01
 
            WRITE FILEOUT01
+           
            GO TO P00.
        P99.
            CLOSE CAREFILE GARFILE FILEOUT
