35704bfacaece28eed474ed3b1acb480dc3c999c
unknown <waitesid@gmail.com>
2020-07-23 12:00:32 -0400

    add search in gnu 52

--- gnu/cobol/sidr052.cob
+++ gnu/cobol/sidr052.cob
@@ -21,7 +21,10 @@
        01  FILEOUT01 PIC X(80).
        FD  PROCFILE.
        01  PROCFILE01.
-           02 PROC-KEY PIC X(11).
+           02 PROC-KEY.
+               03 PROC-CDM PIC X(4).
+               03 PROC-CPT PIC X(5).
+               03 PROC-MOD PIC X(2).
            02 PROC-TYPE PIC X.
            02 PROC-TITLE PIC X(28).
            02 PROC-AMOUNT PIC 9(4)V99.
@@ -55,17 +58,18 @@
            02 FILLER PIC XXX VALUE SPACES.
            02 O2F2 PIC X(18).
        01  NUM-2 PIC 99.
-       01   RIGHT-2 PIC XX JUST RIGHT.
-       01   RIGHT-4 PIC XXXX JUST RIGHT.
-       01   ALF-7 PIC X(8).
-       01   CENTS PIC XX.
-       01   SIGN-DOLLAR PIC XXXX.
-       01   X PIC 99.
-       01   NEF-7 PIC ZZ,ZZ9.99.
-       01   ANS PIC X(32).
-       01   FUNC PIC X(8).
-       01   ALF-6 PIC X(6).
-       01   NUM-6 PIC 9(6).
+       01  RIGHT-2 PIC XX JUST RIGHT.
+       01  RIGHT-4 PIC XXXX JUST RIGHT.
+       01  ALF-7 PIC X(8).
+       01  CENTS PIC XX.
+       01  SIGN-DOLLAR PIC XXXX.
+       01  X PIC 99.
+       01  NEF-7 PIC ZZ,ZZ9.99.
+       01  ANS PIC X(32).
+       01  FUNC PIC X(8).
+       01  ALF5 PIC X(5).
+       01  ALF-6 PIC X(6).
+       01  NUM-6 PIC 9(6).
        01  ALF4 PIC XXXX.
        01  ALF7 PIC X(7).
        01  ALF1 PIC X.
@@ -81,39 +85,86 @@
            OPEN INPUT PROCFILE.
            OPEN OUTPUT FILEOUT.
            OPEN INPUT BILLPARM.
-           READ BILLPARM AT END GO TO S1.
+           READ BILLPARM AT END
+               GO TO S1
+           END-READ.    
        S1. 
-           DISPLAY "FUNCTION ?".
-           ACCEPT ANS.
-           IF ANS = "END" GO TO Z1.
+           DISPLAY "FUNCTION ?"
+           ACCEPT ANS
+           
+           IF ANS = "END"
+               GO TO Z1
+           END-IF
+
            IF ANS = "?"
-           DISPLAY "A = ADD A PROCEDURE CODE"
-           DISPLAY "C = CHANGE A PROCEDURE CODE"
-           DISPLAY "D = DELETE A PROCEDURE CODE(BEWARE)"
-           DISPLAY "L = LIST A SINGLE PROCEDURE CODE"
-           DISPLAY "F = LIST 20 PROCEDURE CODES AT A TIME"
-           DISPLAY "    STARTING WITH CODE AFTER THE ONE TYPED IN"
-           DISPLAY "PFS = PRINT FEE SCHEDULE TO LINE PRINTER"
-           DISPLAY "END = END THE PROGRAM"
-           GO TO S1.
-           MOVE SPACES TO FUNC PROC-KEY.
-           UNSTRING ANS DELIMITED BY "," INTO FUNC PROC-KEY.
-           IF FUNC = "D" OR "A" OR "C" OR "L" OR "F" OR "PFS"
-           NEXT SENTENCE ELSE DISPLAY "WHAT?" GO TO S1.
+               DISPLAY "A = ADD A PROCEDURE CODE"
+               DISPLAY "C = CHANGE A PROCEDURE CODE"
+               DISPLAY "D = DELETE A PROCEDURE CODE(BEWARE)"
+               DISPLAY "L = LIST A SINGLE PROCEDURE CODE"
+               DISPLAY "F = LIST 20 PROCEDURE CODES AT A TIME"
+               DISPLAY "S = search by CPT"    
+               DISPLAY "    STARTING WITH CODE AFTER THE ONE TYPED IN"
+               DISPLAY "PFS = PRINT FEE SCHEDULE TO LINE PRINTER"
+               DISPLAY "END = END THE PROGRAM"
+               GO TO S1
+           END-IF
+
+           MOVE SPACES TO FUNC PROC-KEY
+           UNSTRING ANS DELIMITED BY "," INTO FUNC PROC-KEY
+           IF FUNC = "D" OR "A" OR "C" OR "L" OR "F" OR "S" OR "PFS"
+               NEXT SENTENCE 
+           ELSE 
+               DISPLAY "WHAT?"
+               GO TO S1
+           END-IF
+
            IF PROC-KEY = "1" OR "2" OR "3" OR "4" OR "5" OR "6"
-           OR "7" OR "8" OR "9" MOVE PROC-KEY TO ALF1
-           MOVE ALF1 TO NUM1
-           MOVE PROCTAB(NUM1) TO PROC-KEY.
-           IF FUNC = "L" PERFORM L1 GO TO S1.
-           IF FUNC = "A" GO TO A1.
-           IF FUNC = "C" PERFORM L1 GO TO C1.
-           IF FUNC = "D" GO TO D1.
-           IF FUNC = "F" GO TO F1.
-           IF FUNC = "PFS" GO TO PFS1.
+                         OR "7" OR "8" OR "9"
+               MOVE PROC-KEY TO ALF1
+               MOVE ALF1 TO NUM1
+               MOVE PROCTAB(NUM1) TO PROC-KEY
+           END-IF
+           
+           IF FUNC = "L"
+               PERFORM L1
+               GO TO S1
+           END-IF
+
+           IF FUNC = "A"
+               GO TO A1
+           END-IF
+
+           IF FUNC = "C"
+               PERFORM L1
+               GO TO C1
+           END-IF
+
+           IF FUNC = "D"
+               GO TO D1
+           END-IF
+
+           IF FUNC = "F"
+               GO TO F1
+           END-IF
+
+           IF FUNC = "S"
+               GO TO ST1
+           END-IF    
+              
+           IF FUNC = "PFS"
+               GO TO PFS1
+           END-IF.    
        L1. 
-           READ PROCFILE INVALID DISPLAY "NOT ON FILE" GO TO S1.
+           READ PROCFILE INVALID
+               DISPLAY "NOT ON FILE"
+               GO TO S1
+           END-READ
+
            MOVE PROC-AMOUNT TO NEF-7.
-           DISPLAY PROC-KEY " " PROC-TYPE " " PROC-TITLE " " NEF-7.
+           DISPLAY PROC-CDM " " 
+                   PROC-CPT ":"
+                   PROC-MOD " "
+                   PROC-TYPE " " PROC-TITLE " " NEF-7.
        F1. 
            MOVE 0 TO X.
            MOVE SPACE TO ANS.
@@ -131,80 +182,130 @@
                ACCEPT ANS
                MOVE 0 TO X
            END-IF
-           IF ANS NOT = SPACE GO TO S1.
+           
+           IF ANS NOT = SPACE
+               GO TO S1
+           END-IF
+
            ADD 1 TO X
            MOVE PROC-KEY TO PROCTAB(X).
            DISPLAY X " " PROC-KEY " " PROC-TYPE " " PROC-TITLE " " 
            NEF-7.
            GO TO F2.
        A1. 
-           READ PROCFILE INVALID GO TO A4.
-           DISPLAY "ALREADY ON FILE" PERFORM L1 GO TO S1.
+           READ PROCFILE INVALID
+               GO TO A4
+           END-READ
+
+           DISPLAY "ALREADY ON FILE"
+           PERFORM L1 
+           GO TO S1.
        A4. 
            DISPLAY "ENTER THE TITLE FOR THE PROCEDURE".
            ACCEPT PROC-TITLE.
            IF PROC-TITLE = "?"
-           DISPLAY "X = BACK TO FUNCTION; NO ADD"
-           DISPLAY "TYPE IN UP TO 32 CHARACTER TITLE FOR"
-           DISPLAY "PROCEDURE"
-           DISPLAY "A DISPLAY OF THE FIRST 32 CHARACTERS WILL OCCUR"
-           DISPLAY "FOR YOUR APPROVAL/DISAPPROVAL"
-           GO TO A4.
-           IF PROC-TITLE = "X" DISPLAY "NO UPDATE" GO TO S1.
+               DISPLAY "X = BACK TO FUNCTION; NO ADD"
+               DISPLAY "TYPE IN UP TO 32 CHARACTER TITLE FOR"
+               DISPLAY "PROCEDURE"
+               DISPLAY "A DISPLAY OF THE FIRST 32 CHARACTERS WILL OCCUR"
+               DISPLAY "FOR YOUR APPROVAL/DISAPPROVAL"
+               GO TO A4
+           END-IF
+
+           IF PROC-TITLE = "X"
+               DISPLAY "NO UPDATE"
+               GO TO S1
+           END-IF
+
            DISPLAY PROC-TITLE.
        A5. 
            DISPLAY "OK ?".
            ACCEPT ANS.
            IF ANS = "?"
-           DISPLAY "TYPE Y TO ACCEPT THE TITLE"
-           DISPLAY "TYPE N TO REJECT THIS TITLE"
-           DISPLAY "AND RETURN TO ENTER A NEW TITLE"
-           GO TO A5.
-           IF ANS = "N" GO TO A4.
-           IF ANS NOT = "Y" MOVE "?" TO ANS GO TO A5.
+               DISPLAY "TYPE Y TO ACCEPT THE TITLE"
+               DISPLAY "TYPE N TO REJECT THIS TITLE"
+               DISPLAY "AND RETURN TO ENTER A NEW TITLE"
+               GO TO A5
+           END-IF
+
+           IF ANS = "N"
+               GO TO A4
+           END-IF
+
+           IF ANS NOT = "Y"
+               MOVE "?" TO ANS
+               GO TO A5
+           END-IF.    
        T1. 
-           DISPLAY "TYPE OF SERVICE. 1 OR 5".
+           DISPLAY "TYPE OF SERVICE: 1 OR 5".
            ACCEPT PROC-TYPE.
-           IF PROC-TYPE = "1" OR "5" GO TO A6
-           ELSE DISPLAY "??? " PROC-TYPE.
+           IF PROC-TYPE = "1" OR "5"
+               GO TO A6
+           ELSE 
+               DISPLAY "??? " PROC-TYPE
+           END-IF.            
        A6. 
            DISPLAY "FEE ?".
            ACCEPT ALF-7.
            IF ALF-7 = "?"
-           DISPLAY "BK = BACK TO TITLE"
-           DISPLAY "X = BACK TO FUNCTION; NO UPDATE"
-           DISPLAY "ENTER THE AMOUNT OF THE FEE"
-           DISPLAY "EX: $78.00 = 78.00, OR 78 OR 78."
-           DISPLAY "EX: 70.10 = 70.10  ONLY"
-           GO TO A6.
-           IF ALF-7 = "BK" GO TO A4.
+               DISPLAY "BK = BACK TO TITLE"
+               DISPLAY "X = BACK TO FUNCTION; NO UPDATE"
+               DISPLAY "ENTER THE AMOUNT OF THE FEE"
+               DISPLAY "EX: $78.00 = 78.00, OR 78 OR 78."
+               DISPLAY "EX: 70.10 = 70.10  ONLY"
+               GO TO A6
+           END-IF
+
+           IF ALF-7 = "BK" 
+               GO TO A4
+           END-IF
+
            MOVE SPACES TO SIGN-DOLLAR CENTS.
            UNSTRING ALF-7 DELIMITED BY "." INTO SIGN-DOLLAR CENTS.
-           IF CENTS = SPACES MOVE "00" TO CENTS.
-           IF CENTS NOT NUMERIC MOVE "?" TO ALF-7
-           GO TO A6.
+           IF CENTS = SPACES 
+               MOVE "00" TO CENTS
+           END-IF
+
+           IF CENTS NOT NUMERIC
+               MOVE "?" TO ALF-7
+               GO TO A6
+           END-IF
+
            MOVE SPACES TO RIGHT-4.
            UNSTRING SIGN-DOLLAR DELIMITED BY " " INTO RIGHT-4
            INSPECT RIGHT-4 REPLACING LEADING " " BY "0"
-           IF RIGHT-4 NOT NUMERIC MOVE "?" TO ALF-7
-           GO TO A6.
+           IF RIGHT-4 NOT NUMERIC 
+               MOVE "?" TO ALF-7
+               GO TO A6
+           END-IF
+
            STRING RIGHT-4 CENTS DELIMITED BY "Z" INTO ALF-6
            MOVE ALF-6 TO NUM-6
            DIVIDE NUM-6 BY 100 GIVING PROC-AMOUNT.
            WRITE PROCFILE01 INVALID
-           DISPLAY "RECORD WAS ADDED"
-           DISPLAY "MOMENTS AGO BY ANOTHER USER" PERFORM L1
-           GO TO S1.
+               DISPLAY "RECORD WAS ADDED"
+               DISPLAY "MOMENTS AGO BY ANOTHER USER"
+               PERFORM L1
+               GO TO S1
+           END-WRITE
+
            DISPLAY "RECORD ADDED"
-           PERFORM L1 GO TO S1.
+           PERFORM L1
+           GO TO S1.
        C1. 
            DISPLAY "FIELD # ?".
            ACCEPT ANS.
            IF ANS = "?"
-           DISPLAY "VALID CODES ARE 1 = TITLE 2 = AMOUNT 3 = TYPE"
-           DISPLAY " X = BACK TO FUNCTION"
-           GO TO C1.
-           IF ANS = "X" DISPLAY "NO UPDATE" GO TO S1.
+               DISPLAY "VALID CODES ARE 1 = TITLE 2 = AMOUNT 3 = TYPE"
+               DISPLAY " X = BACK TO FUNCTION"
+               GO TO C1
+           END-IF
+
+           IF ANS = "X" 
+               DISPLAY "NO UPDATE"
+               GO TO S1
+           END-IF
+
            MOVE SPACE TO RIGHT-2.
            UNSTRING ANS DELIMITED BY " " INTO RIGHT-2.
            INSPECT RIGHT-2 REPLACING LEADING " " BY "0".
@@ -352,7 +453,46 @@
            DISPLAY "RECORD CHANGED"
            CLOSE PROCFILE
            OPEN INPUT PROCFILE
-           GO TO S1.    
+           GO TO S1.
+       ST1.
+           MOVE SPACE TO PROC-KEY
+           DISPLAY "Enter a CPT code"
+           ACCEPT ALF5
+
+           IF ALF5(5:1) NOT NUMERIC
+               DISPLAY "Not a valid CPT, try again"
+               GO TO ST1
+           END-IF
+
+           START PROCFILE KEY NOT < PROC-KEY INVALID
+               DISPLAY "END OF FILE"
+               GO TO S1
+           END-START
+           MOVE 0 TO X.
+       ST2.
+           READ PROCFILE NEXT AT END 
+               DISPLAY "END OF FILE" 
+               GO TO S1
+           END-READ               
+              
+           IF PROC-AMOUNT = 0
+               GO TO ST2
+           END-IF
+
+           IF ALF5 NOT = PROC-KEY(5:5)
+               GO TO ST2
+           END-IF
+
+           MOVE PROC-AMOUNT TO NEF-7   
+           DISPLAY PROC-CDM " " PROC-CPT ":" PROC-MOD " "
+                   PROC-TYPE " " PROC-TITLE " " NEF-7
+           ADD 1 TO X
+           IF X > 20
+               ACCEPT OMITTED
+               MOVE 0 TO X
+           END-IF
+           GO TO ST2.
+            
        Z1.
            CLOSE PROCFILE FILEOUT BILLPARM.
            STOP RUN.
