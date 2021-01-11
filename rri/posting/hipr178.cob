1f971fb625365717c86bea8b247abfef73548ad2
stephen waite <stephen.waite@cmsvt.com>
2021-01-11 14:00:27 -0500

    add denial check to bash paid

--- rri/posting/hipr178.cob
+++ rri/posting/hipr178.cob
@@ -1364,10 +1364,19 @@
                      CAS-8 CAS-9 CAS-10 CAS-11 CAS-12 CAS-13 CAS-14 
                      CAS-15 CAS-16 CAS-17 CAS-18 CAS-19
 
-                   IF CAS-2 = "50" OR "5 " OR "234"
-                       MOVE 1 TO FLAG
-                       MOVE Z TO CAS-CNTR
-                   END-IF               
+                 IF  (CAS-2 = "50" OR "109" OR "167" OR "B13")
+                   OR (CAS-1 = "PI" AND CAS-2 = "97   ")
+                   OR (CAS-1 = "CO" AND CAS-2 = "97   ")
+                   OR (CAS-1 = "CO" AND CAS-2 = "197  ")
+                   OR (CAS-1 = "CO" AND CAS-2 = "4    ")
+                   OR (CAS-1 = "CO" AND CAS-2 = "16   ")
+                   OR (CAS-1 = "CO" AND CAS-2 = "18   ")
+                   OR (CAS-1 = "CO" AND CAS-2 = "58   ")
+                   OR (CAS-1 = "CO" AND CAS-2 = "96   ")
+                   OR (CAS-1 = "CO" AND CAS-2 = "55   ")
+                   MOVE 1 TO FLAG
+                   MOVE Z TO CAS-CNTR
+                 END-IF               
                END-IF               
            END-PERFORM.    
 
