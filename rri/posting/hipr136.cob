db2fb831ddef189e7eba5bc770bd3dcba6f279e9
stephen waite <stephen.waite@cmsvt.com>
2021-01-15 13:16:53 -0500

    error lists (#153)

--- rri/posting/hipr136.cob
+++ rri/posting/hipr136.cob
@@ -1140,25 +1140,27 @@
               MOVE SPACE TO CAS01 
               MOVE CAS-TAB(Z) TO FILEIN01
               UNSTRING FILEIN01 DELIMITED BY "*" INTO
-              CAS-0 CAS-1 CAS-2 CAS-3 CAS-4 CAS-5 CAS-6 CAS-7 
-              CAS-8 CAS-9 CAS-10 CAS-11 CAS-12 CAS-13 CAS-14 
-              CAS-15 CAS-16 CAS-17 CAS-18 CAS-19
-               IF  (CAS-2 = "50" OR "109" OR "167" OR "B13")
-                 OR (CAS-1 = "PI" AND CAS-2 = "97   ")
-                 OR (CAS-1 = "CO" AND CAS-2 = "97   ")
-                 OR (CAS-1 = "CO" AND CAS-2 = "197  ")
-                 OR (CAS-1 = "CO" AND CAS-2 = "4    ")
-                 OR (CAS-1 = "CO" AND CAS-2 = "16   ")
-                 OR (CAS-1 = "CO" AND CAS-2 = "18   ")
-                 OR (CAS-1 = "CO" AND CAS-2 = "58   ")
-                 OR (CAS-1 = "CO" AND CAS-2 = "96   ")
-                 OR (CAS-1 = "CO" AND CAS-2 = "55   ")
-                 OR (CAS-1 = "PR" AND CAS-2 = "31   ")
-                 OR (CAS-1 = "PR" AND CAS-2 = "96   ")
-                    MOVE 1 TO FLAG
-                    MOVE CAS-CNTR TO Z
-                    GO TO DUMP50-EXIT
-               END-IF
+                CAS-0 CAS-1 CAS-2 CAS-3 CAS-4 CAS-5 CAS-6 CAS-7 
+                CAS-8 CAS-9 CAS-10 CAS-11 CAS-12 CAS-13 CAS-14 
+                CAS-15 CAS-16 CAS-17 CAS-18 CAS-19
+              IF (CAS-2 = "50" OR "109" OR "167" OR "B13")
+                OR (CAS-1 = "CO" AND CAS-2 = "4    ")
+                OR (CAS-1 = "CO" AND CAS-2 = "16   ")
+                OR (CAS-1 = "CO" AND CAS-2 = "18   ")
+                OR (CAS-1 = "CO" AND CAS-2 = "29   ")
+                OR (CAS-1 = "CO" AND CAS-2 = "55   ")
+                OR (CAS-1 = "CO" AND CAS-2 = "58   ")
+                OR (CAS-1 = "CO" AND CAS-2 = "96   ")
+                OR (CAS-1 = "CO" AND CAS-2 = "97   ")
+                OR (CAS-1 = "CO" AND CAS-2 = "197  ")
+                OR (CAS-1 = "PI" AND CAS-2 = "97   ")
+                OR (CAS-1 = "PR" AND CAS-2 = "31   ")
+                OR (CAS-1 = "PR" AND CAS-2 = "31   ")
+                OR (CAS-1 = "PR" AND CAS-2 = "96   ")
+                MOVE 1 TO FLAG
+                MOVE CAS-CNTR TO Z
+                GO TO DUMP50-EXIT
+              END-IF
             END-IF.
        DUMP50-EXIT.
             EXIT.      
--- rri/posting/hipr178.cob
+++ rri/posting/hipr178.cob
@@ -1399,18 +1399,22 @@
                      CAS-8 CAS-9 CAS-10 CAS-11 CAS-12 CAS-13 CAS-14 
                      CAS-15 CAS-16 CAS-17 CAS-18 CAS-19
 
-                 IF  (CAS-2 = "50" OR "109" OR "167" OR "B13")
-                   OR (CAS-1 = "PI" AND CAS-2 = "97   ")
-                   OR (CAS-1 = "CO" AND CAS-2 = "97   ")
-                   OR (CAS-1 = "CO" AND CAS-2 = "197  ")
+                 IF (CAS-2 = "50" OR "109" OR "167" OR "B13")
                    OR (CAS-1 = "CO" AND CAS-2 = "4    ")
                    OR (CAS-1 = "CO" AND CAS-2 = "16   ")
                    OR (CAS-1 = "CO" AND CAS-2 = "18   ")
+                   OR (CAS-1 = "CO" AND CAS-2 = "29   ")
+                   OR (CAS-1 = "CO" AND CAS-2 = "55   ")
                    OR (CAS-1 = "CO" AND CAS-2 = "58   ")
                    OR (CAS-1 = "CO" AND CAS-2 = "96   ")
-                   OR (CAS-1 = "CO" AND CAS-2 = "55   ")
+                   OR (CAS-1 = "CO" AND CAS-2 = "97   ")
+                   OR (CAS-1 = "CO" AND CAS-2 = "197  ")
+                   OR (CAS-1 = "PI" AND CAS-2 = "97   ")
+                   OR (CAS-1 = "PR" AND CAS-2 = "31   ")
+                   OR (CAS-1 = "PR" AND CAS-2 = "31   ")
+                   OR (CAS-1 = "PR" AND CAS-2 = "96   ")                  
                    MOVE 1 TO FLAG
-                   MOVE Z TO CAS-CNTR
+                   MOVE CAS-CNTR TO Z
                  END-IF               
                END-IF               
            END-PERFORM.    
