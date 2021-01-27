49c12685f541bf9b7331bb24be15c4cebfc10086
unknown <waitesid@gmail.com>
2021-01-27 18:04:14 -0500

    update 003 fees

--- nonrri/fees/care008.cob
+++ nonrri/fees/care008.cob
@@ -4,39 +4,43 @@
       * @copyright Copyright (c) 2020 cms <cmswest@sover.net>
       * @license https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
        IDENTIFICATION DIVISION.
-       PROGRAM-ID. med008.
-       AUTHOR. SID WAITE.
+       PROGRAM-ID. care008.
+       AUTHOR. SWAITE.
        DATE-COMPILED. TODAY.
        ENVIRONMENT DIVISION.
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
-           SELECT MEDFILE2020 ASSIGN TO "S30" ORGANIZATION IS INDEXED
-           ACCESS MODE IS DYNAMIC RECORD KEY IS MED-KEY
-           LOCK MODE MANUAL.
+
+           SELECT  medfile ASSIGN TO "S30" ORGANIZATION IS INDEXED
+             ACCESS MODE IS DYNAMIC RECORD KEY IS MED-KEY
+             LOCK MODE MANUAL.
+
            SELECT FILEIN ASSIGN TO "S35"
-           ORGANIZATION LINE SEQUENTIAL.
+             ORGANIZATION LINE SEQUENTIAL.
+
        DATA DIVISION.
+       
        FILE SECTION.
+
        FD FILEIN.
-       01  FILEIN01.
-           02 FI-1 PIC X.
-           02 FILLER PIC X(7).
-           02 FI-2 PIC X(5).
-           02 FILLER PIC X(3).
-           02 FI-3 PIC XX.
-           02 FILLER PIC X(6).
-           02 FI-4 PIC X(7).
-       FD MEDFILE2020.
-       01 MEDFILE202001.
+       01  FILEIN01 pic x(120).
+           
+       FD  medfile.
+       01  medfile01.
            02 MED-KEY.
              03 MED-KEY1 PIC X(5).
              03 MED-KEY2 PIC XX.
            02 MED-AMT PIC 9(4)V99.
            
        WORKING-STORAGE SECTION.
+
+       01  FI-1 PIC X.
        01  FI-CPT PIC X(5).
        01  FI-MOD PIC XX.
        01  FI-AMT PIC X(7).
+       01  FI-NONPAR PIC X(7).
+       01  FI-LIMIT PIC X(7).
+       01  FI-EFFDATE PIC X(10).
        01  NUM2 PIC 99.
        01  NUM4 PIC 9999.
        01  ALF2 PIC XX.
@@ -45,21 +49,31 @@
        01  CENTX PIC XX.
        01  right-4 pic xxxx just right.
        01  X-AMT PIC 9(4)V99.
+
        PROCEDURE DIVISION.
+       
        P0.
-           open output MEDFILE2020
-           close MEDFILE2020
+           open output medfile
+           close medfile
            OPEN INPUT FILEIN.
-           OPEN I-O MEDFILE2020.
+           OPEN I-O medfile.
 
        P1.
-           READ FILEIN AT END GO TO P99.
-           MOVE FI-4 TO FI-AMT
-           MOVE FI-3 TO FI-MOD
-           MOVE FI-2 TO FI-CPT
+           READ FILEIN 
+             AT END 
+               GO TO P99.
+
+           MOVE SPACE TO FI-1 FI-CPT FI-MOD FI-AMT.    
+
+           UNSTRING FILEIN01 DELIMITED BY "," INTO     
+             FI-1 FI-CPT FI-MOD FI-AMT
+
            IF FI-1 = "C" GO TO P1.
+
            IF FI-1 = "#" GO TO P1.
+
            IF FI-MOD = "TC" GO TO P1.
+
        P1-1.
            MOVE SPACE TO RIGHT-4 CENTX
            UNSTRING FI-AMT DELIMITED BY "." INTO right-4 CENTX
@@ -67,10 +81,12 @@
            MOVE FI-MOD TO MED-KEY2
            INSPECT RIGHT-4 REPLACING ALL " " BY "0".
            INSPECT CENTX REPLACING  ALL " " BY "0".
+
            IF CENTX NOT NUMERIC
              DISPLAY FI-CPT " CENTX NOT NUM " CENTX
              ACCEPT OMITTED
            END-IF
+           
            IF RIGHT-4 NOT NUMERIC
              DISPLAY FI-CPT " RIGHT-4 NOT NUM " RIGHT-4
              ACCEPT OMITTED
@@ -81,8 +97,10 @@
       *     display right-4
       *     display centx
            COMPUTE MED-AMT = num4 + (num2 / 100)
-           WRITE MEDFILE202001
+           WRITE medfile01
            GO TO P1.
-       P99.
-           CLOSE MEDFILE2020 FILEIN.
+
+       
+       P99.           
+           CLOSE medfile FILEIN.
            STOP RUN.
