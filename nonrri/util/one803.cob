a4a61eba4d417721a7164195620fb81be42a06db
stephen waite <stephen.waite@cmsvt.com>
2021-02-16 09:35:43 -0500

    mips365 using one803

--- nonrri/util/one803.cob
+++ nonrri/util/one803.cob
@@ -33,77 +33,22 @@
                ALTERNATE RECORD KEY IS INS-NEIC-ASSIGN WITH DUPLICATES
                LOCK MODE MANUAL.
 
+           SELECT GARFILE ASSIGN TO "S50" ORGANIZATION IS INDEXED
+             ACCESS MODE IS DYNAMIC  RECORD KEY IS G-GARNO
+             LOCK MODE MANUAL.    
+
+           SELECT MIPSOUT ASSIGN TO "S55" ORGANIZATION LINE SEQUENTIAL.
+  
+
        DATA DIVISION.
        FILE SECTION.
+       
        FD  CHARCUR.
-       01  CHARCUR01.
-           02 CHARCUR-KEY.
-             03 CC-KEY8 PIC X(8).
-             03 CC-KEY3 PIC XXX.
-           02 CC-PATID PIC X(8).
-           02 CC-CLAIM PIC X(6).
-           02 CC-SERVICE PIC X.
-           02 CC-DIAG PIC X(7).
-           02 CC-PROC PIC X(7).
-           02 CC-MOD2 PIC XX.
-           02 CC-MOD3 PIC XX.
-           02 CC-MOD4 PIC XX.
-           02 CC-AMOUNT PIC S9(4)V99.
-           02 CC-DOCR PIC X(3).
-           02 CC-DOCP PIC X(2).
-           02 CC-PAYCODE PIC XXX.
-      * 60     
-           02 CC-STUD PIC X.
-           02 CC-WORK PIC XX.
-           02 CC-DAT1 PIC X(8).
-           02 CC-RESULT PIC X.
-           02 CC-ACT PIC X.
-           02 CC-SORCREF PIC X.
-           02 CC-COLLT PIC X.
-           02 CC-AUTH PIC X.
-           02 CC-PAPER PIC X.
-           02 CC-PLACE PIC X.
-           02 CC-EPSDT PIC X.
-           02 CC-DATE-T PIC X(8).
-           02 CC-DATE-A PIC X(8).
-           02 CC-DATE-P PIC X(8).
-           02 CC-REC-STAT PIC X.
-      * 104     
-           02 CC-DX2 PIC X(7).
-           02 CC-DX3 PIC X(7).
-           02 CC-ACC-TYPE PIC X.
-           02 CC-DATE-M PIC X(8).
-           02 CC-ASSIGN PIC X.
-           02 CC-NEIC-ASSIGN PIC X.
-           02 CC-DX4 PIC X(7).
-           02 CC-DX5 PIC X(7).
-           02 CC-DX6 PIC X(7).
-      * 150     
-           02 CC-FUTURE PIC X(6).
+       copy charcur.cpy in "c:\users\sid\cms\copylib".
        
        FD  INSFILE.
-       01  INSFILE01.
-           02 INS-KEY PIC XXX.
-           02 INS-NAME PIC X(22).
-           02 INS-STREET PIC X(24).
-           02 INS-CITY PIC X(15).
-           02 INS-STATE PIC XX.
-           02 INS-ZIP PIC X(9).
-           02 INS-ASSIGN PIC X.
-           02 INS-CLAIMTYPE PIC X.
-           02 INS-NEIC PIC X(5).
-           02 INS-NEICLEVEL PIC X.
-           02 INS-NEIC-ASSIGN PIC X.
-           02 INS-PPO PIC X.
-           02 INS-PRVNUM PIC X(10).
-           02 INS-HMO PIC X(3).
-           02 INS-STATUS PIC X.
-           02 INS-LEVEL PIC X.
-           02 INS-LASTDATE PIC X(8).
-           02 INS-CAID PIC XXX.
-           02 INS-REFWARN PIC X.
-           02 INS-FUTURE PIC X(8).
-
+       copy insfile.cpy in "c:\users\sid\cms\copylib".
+           
        FD  FILEIN.
        01  FILEIN01. 
            02 FI-1 PIC X(5).      
@@ -116,14 +61,33 @@
        FD  AGEDATE.
        01  AGEDATE01.
            02 DATE-LOW PIC X(8).
-           02 DATE-HIGH PIC X(8).           
+           02 DATE-HIGH PIC X(8).      
+
+       FD  GARFILE.
+       copy garfile.cpy in "c:\users\sid\cms\copylib".
+
+       FD  MIPSOUT.
+       01  MIPSOUT01.       
+           02 KEY-OUT PIC X(11).
+           02 PROV-FNAME PIC X(20).
+           02 PROV-LNAME PIC X(20).
+           02 PAT-FNAME PIC X(20).
+           02 PAT-LNAME PIC X(20).
+           02 PAT-DOB PIC X(20).
+           02 PAT-SEX PIC X(20).
+           02 DATE1 PIC X(20).
+           02 DESC  PIC X(20).
+           02 F-ICD PIC X(11).
+           02 ICD10 PIC X(20).
+           02 F-PROC PIC X(7).
 
        WORKING-STORAGE SECTION. 
 
        PROCEDURE DIVISION.
+
        P0.
-           OPEN INPUT AGEDATE CHARCUR FILEIN INSFILE
-           OPEN OUTPUT FILEOUT.
+           OPEN INPUT AGEDATE CHARCUR FILEIN INSFILE garfile
+           OPEN OUTPUT FILEOUT MIPSOUT.
 
            READ AGEDATE.
 
@@ -167,8 +131,50 @@
 
            WRITE FILEOUT01
 
+           MOVE CC-KEY8 TO G-GARNO
+           READ GARFILE 
+             INVALID 
+               DISPLAY G-GARNO " HAS NO ACCT"
+               ACCEPT ALF1
+               GO TO P2
+           END-READ.
+
+           MOVE SPACE TO PROV-FNAME PROV-LNAME
+           MOVE "DANIEL" TO PROV-FNAME
+           MOVE "MCCAULIFFE" TO PROV-LNAME
+           MOVE SPACE TO PAT-LNAME PAT-FNAME
+
+           IF CC-DOCP = "02"
+             MOVE "KERRY" TO PROV-FNAME
+             MOVE "LANE" TO PROV-LNAME.
+
+           UNSTRING G-GARNAME DELIMITED BY ";"
+             INTO PAT-LNAME PAT-FNAME ALF1
+           
+           MOVE SPACE TO PAT-DOB
+           STRING G-DOB(5:2) "/" G-DOB(7:2) "/" G-DOB(1:4)
+           DELIMITED BY SIZE INTO PAT-DOB
+           MOVE SPACE TO PAT-SEX
+           MOVE "MALE  " TO PAT-SEX
+           IF G-SEX = "F"
+           MOVE "FEMALE" TO PAT-SEX.
+           MOVE DX-0 TO DIAG-KEY
+           READ DIAGFILE INVALID 
+             MOVE DX-0 TO ICD10
+            NOT INVALID
+             MOVE DX-0 TO ICD10
+           END-READ
+           MOVE SPACE TO DATE1
+           STRING CC-DATE-T(5:2) "/" CC-DATE-T(7:2) "/" CC-DATE-T(1:4)
+           DELIMITED BY SIZE INTO DATE1
+           MOVE CHARCUR-KEY TO KEY-OUT
+           MOVE "ICD10" TO DESC
+           MOVE CC-PROC TO F-PROC
+           MOVE "5050F  " TO F-ICD
+           WRITE MIPSOUT01
+
            GO TO P2.
            
        P9.
-           CLOSE AGEDATE CHARCUR FILEOUT FILEIN INSFILE
+           CLOSE AGEDATE CHARCUR FILEOUT FILEIN INSFILE GARFILE MIPSOUT
            STOP RUN.
