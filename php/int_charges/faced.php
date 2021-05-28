ab093c14aec28e001b26dfe29f0d1373fadc795f
stephen waite <stephen.waite@cmsvt.com>
2021-05-28 15:21:49 -0400

    strip out necc fields from rrmc

---
+++ php/int_charges/faced.php
@@ -0,0 +1,31 @@
+<?php
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+use League\Csv\Reader;
+
+$reader = Reader::createFromPath('/tmp/test.csv', 'r');
+$reader->setHeaderOffset(0);
+
+$records = $reader->getRecords();
+foreach ($records as $offset => $record) {
+    //$offset : represents the record offset
+    //var_export($record); // returns something like
+    //var_dump($record);
+    //if (in_array($record['Date'], $record)) {
+    //echo $record['Date'] . "\n";
+    //echo implode('', array_reverse(explode('/', $record['Date']))) . "\n";
+    if ($record['HCPCS'] == "G1004") {
+      $cpt = "G1004";
+    }
+        $cdm = $record['CDM'];
+        $cpt = $record['CPT'];
+        $dos = $record['DOS'];
+        $mrn = $record['MRN'];
+        if ($record['HCPCS'] == "G1004") {
+          $cpt = "G1004";
+        }
+        echo $cdm . "," . $cpt . "," . $dos . "," . $mrn . "\n";
+}
+
+
