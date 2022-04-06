c25b32361f56f4415f8e20a7d498e560cab3de34
stephen waite <stephen.waite@cmsvt.com>
2022-04-06 16:32:19 -0400

    now for 002

--- rri/coding/pas002.cob
+++ rri/coding/pas002.cob
@@ -22,70 +22,52 @@
                ALTERNATE RECORD KEY IS G-ACCT WITH DUPLICATES
                LOCK MODE MANUAL.               
 
+           SELECT EMAILAUTHFILE ASSIGN TO "S45"
+               ORGANIZATION IS INDEXED
+               ACCESS MODE DYNAMIC RECORD KEY IS EA-KEY
+               ALTERNATE RECORD KEY IS EA-MEDREC WITH DUPLICATES
+               ALTERNATE RECORD KEY IS EA-NAME WITH DUPLICATES
+               ALTERNATE RECORD KEY IS EA-EMAIL WITH DUPLICATES
+               ALTERNATE RECORD KEY IS EA-AUTH WITH DUPLICATES
+               ALTERNATE RECORD KEY IS EA-DATE-E WITH DUPLICATES
+               ALTERNATE RECORD KEY IS EA-SSN WITH DUPLICATES
+               LOCK MODE MANUAL.
+
+           SELECT AUTHFILE ASSIGN TO "S50" ORGANIZATION IS INDEXED
+               ACCESS IS DYNAMIC RECORD KEY IS AUTH-KEY
+               LOCK MODE MANUAL
+               STATUS IS AUTHFILE-STAT.
+
        DATA DIVISION.
        FILE SECTION.
 
        FD  CHARFILE
-           DATA RECORD IS CHARFILE01.
-       01  CHARFILE01.
-           02 CHARFILE-KEY.
-             03 CD-KEY8 PIC X(8).
-             03 CD-KEY3 PIC XXX.
-           02 CD-PATID PIC X(8).
-           02 CD-CLAIM PIC X(6).
-           02 CD-SERVICE PIC X.
-           02 CD-DIAG PIC X(7).
-           02 CD-PROC. 
-               03 CD-PROC0 PIC X(4).
-               03 CD-PROC1 PIC X(5).
-               03 CD-PROC3 PIC XX.
-           02 CD-MOD2 PIC XX.
-           02 CD-MOD3 PIC XX.
-           02 CD-MOD4 PIC XX.
-           02 CD-AMOUNT PIC S9(4)V99.
-           02 CD-DOCR PIC X(3).
-           02 CD-DOCP PIC X(2).
-           02 CD-PAYCODE PIC XXX.
-           02 CD-STAT PIC X.
-           02 CD-WORK PIC XX.
-           02 CD-DAT1 PIC X(8).
-           02 CD-RESULT PIC X.
-           02 CD-ACT PIC X.
-           02 CD-SORCREF PIC X.
-           02 CD-COLLT PIC X.
-           02 CD-AUTH PIC X.
-           02 CD-PAPER PIC X.
-           02 CD-PLACE PIC X.
-           02 CD-NAME PIC X(24).
-           02 CD-ESPDT PIC X.
-           02 CD-DATE-T PIC X(8).
-           02 CD-DATE-E PIC X(8).
-           02 CD-ORDER PIC X(6).
-           02 CD-DX2 PIC X(7).
-           02 CD-DX3 PIC X(7).
-           02 CD-DATE-A PIC X(8).
-           02 CD-ACC-TYPE PIC X.
-           02 CD-DATE-M PIC X(8).
-           02 CD-ASSIGN PIC X.
-           02 CD-NEIC-ASSIGN PIC X.
-           02 CD-DX4 PIC X(7).
-           02 CD-DX5 PIC X(7).
-           02 CD-DX6 PIC X(7).
-           02 CD-FUTURE PIC X(6).
-
+           copy charfile.cpy in "c:\users\sid\cms\copylib\rri".
+       
        FD  FILEOUT.
        01  FILEOUT01 PIC X(189). 
 
        FD  GARFILE.
-           COPY garfile.CPY IN "C:\Users\sid\cms\copylib\rri".  
+           COPY garfile.CPY IN "C:\Users\sid\cms\copylib\rri".
+
+       FD  EMAILAUTHFILE.
+           copy emailauthfile.cpy in "c:\users\sid\cms\copylib\rri". 
+
+       FD  AUTHFILE.
+           copy authfile.cpy in "c:\users\sid\cms\copylib\rri".      
 
        WORKING-STORAGE SECTION.
-       01  HOLDIT PIC X(8).
-       01  ANS PIC X.
-      *
+       01  AUTHFILE-STAT PIC XX.
+       01  AUTHFILE-BACK PIC X(91).
+       01  HOLD-AUTH PIC X(20).
+       01  VALIDATE-FLAG PIC 9.
+       01  HOLD-AUTH-PIC9 PIC 9(10).    
+
+       
        PROCEDURE DIVISION.
        P0.
-           OPEN INPUT CHARFILE GARFILE
+           OPEN INPUT EMAILAUTHFILE AUTHFILE.
+           OPEN I-O CHARFILE GARFILE.
            OPEN OUTPUT FILEOUT.
            MOVE SPACE TO CHARFILE-KEY.
 
@@ -99,11 +81,6 @@
 
            IF CD-PLACE NOT = "5" GO TO P1.
 
-           MOVE CD-KEY8 TO G-GARNO
-           READ GARFILE INVALID               
-             GO TO P1
-           END-READ
-
       *    The State of Vermont Total Choice Plan (prefix FVT)
       *    doesn't require prior approval
            IF G-PRIPOL(1:3) = "FVT" GO TO P1.
@@ -111,7 +88,7 @@
       *    02 pa guide attachment III
       *    ct colonography
            IF CD-PROC1 = "74261" OR "74262" OR "74263"
-             GO TO P2.
+             GO TO find-auth.
 
       *    02 CT Scans       
            IF CD-PROC1 = "70450" OR "70460" OR "70470" OR "70480" OR
@@ -123,7 +100,7 @@
              "73201" OR "73202" OR "73700" OR "73701" OR "73702" OR 
              "74150" OR "74160" OR "74170" OR "74176" OR "74177" OR
              "74178" OR "75571" OR "75572" OR "75573" OR "77078"
-             GO TO P2.  
+             GO TO find-auth.  
 
       *    Magnetic Resonance Imaging (MRI)     
            IF CD-PROC1 = "70336" OR "70540" OR "70542" OR "70543" OR
@@ -137,56 +114,155 @@
              "74712" OR "74713" OR "75557" OR "75559" OR "75561" OR
              "75563" OR "75565" OR "76390" OR "76391" OR "77046" OR
              "77047" OR "77048" OR "77049" OR "77084" 
-             GO TO P2.
+             GO TO find-auth.
              
       *    Positron Emission Tomography (PET) Scans
            IF CD-PROC1 = "78459" OR "78491" OR "78429" OR "78430" OR
              "78431" OR "78432" OR "78433" OR "78434" OR "78492" OR 
              "78608" OR "78609" OR "78811" OR "78812" OR "78813" OR 
              "78814" OR "78815" OR "78816" 
-             GO TO P2.  
+             GO TO find-auth.  
                
       *    Single-Photon Emission Computed Tomography
            IF CD-PROC1 = "78803" OR "78830" OR "78831" OR "78832"
-             GO TO P2.
+             GO TO find-auth.
 
       *    Attachment V, BCBSVT members
       *    Cardiac blood pool imaging
            IF CD-PROC1 = "78472" OR "78473" OR "78481" OR "78483" OR
              "78494"
-             GO TO P3.
+             GO TO find-auth.
 
       *    CTA Scans not in attachment III
            IF CD-PROC1 = "70496" OR "70498" OR "71275" OR "72191" OR
              "73206" OR "73706" OR "74174" OR "74175" OR "75574" OR
              "75635"
-             GO TO P3.  
+             GO TO find-auth.  
 
       *    MRA Scans not in attachment III
            IF CD-PROC1 = "70544" OR "70545" OR "70546" OR "70547" OR
              "70548" OR "70549" OR "71555" OR "72159" OR "72198" OR
              "73225" OR "73725" OR "74185"
-             GO TO P3.
+             GO TO find-auth.
 
            GO TO P1.
+
+       find-auth.
+           MOVE CD-KEY8 TO G-GARNO
+           READ GARFILE WITH LOCK
+             INVALID
+               move space to fileout01
+               string "COULDN'T READ GARFILE WITH LOCK " cd-key8
+               delimited by size into FILEOUT01
+               write fileout01
+               GO TO P1
+           END-READ
+
+           MOVE SPACE TO HOLD-AUTH
+           MOVE 0 TO VALIDATE-FLAG
+           move g-acct to ea-medrec
+           start emailauthfile key not > ea-medrec
+             invalid
+               PERFORM P3
+               GO TO P1
+             not invalid
+               perform emailauth-1 thru emailauth-exit
+           end-start
+
+           GO TO P1.
+
+       emailauth-1.           
+           read emailauthfile previous
+             at end
+               PERFORM P3
+               go to emailauth-exit.    
+
+           if ea-medrec not = g-acct
+             PERFORM P3
+             go to emailauth-exit.
+
+           IF EA-DATE-E = CD-DATE-T
+               PERFORM VALIDATE-AUTH-NUM THRU VALIDATE-AUTH-NUM-EXIT
+               
+               IF VALIDATE-FLAG = 1
+                 PERFORM ADD-AUTH
+                 PERFORM P2
+                 go to emailauth-exit
+               END-IF
+
+           END-IF    
+
+           go to emailauth-1.
+
+       emailauth-exit.
+           exit.
+
+       VALIDATE-AUTH-NUM.
+           MOVE EA-AUTH TO HOLD-AUTH
+           
+           MOVE HOLD-AUTH(1:9) TO HOLD-AUTH-PIC9
+
+           IF HOLD-AUTH-PIC9 NOT NUMERIC
+               GO TO VALIDATE-AUTH-NUM-EXIT               
+           END-IF
+
+           MOVE 1 TO VALIDATE-FLAG.
+
+       VALIDATE-AUTH-NUM-EXIT.
+           EXIT.   
+
+       ADD-AUTH.
+           STRING CD-KEY8 CD-CLAIM DELIMITED BY SIZE INTO AUTH-KEY
+           move HOLD-AUTH TO AUTH-NUM
+           MOVE EA-DATE-E TO AUTH-DATE-E
+           MOVE AUTHFILE01 TO AUTHFILE-BACK
+           PERFORM WRITE-AU THRU WRITE-AU-EXIT 
+           MOVE 1 TO CD-AUTH
+           REWRITE CHARFILE01.
+
+       WRITE-AU.
+           CLOSE AUTHFILE
+           OPEN I-O AUTHFILE
+           MOVE AUTHFILE-BACK TO AUTHFILE01
+           WRITE AUTHFILE01 INVALID
+                MOVE SPACE TO FILEOUT01
+                STRING AUTHFILE01 " RECORD NOT ADDED AT THIS TIME " 
+                    AUTHFILE-STAT " STAT" DELIMITED BY SIZE
+                    INTO FILEOUT01 
+                WRITE FILEOUT01    
+                CLOSE AUTHFILE
+                OPEN INPUT AUTHFILE
+                GO TO WRITE-AU-EXIT
+           END-WRITE
+           
+           CLOSE AUTHFILE
+           OPEN INPUT AUTHFILE.
+
+       WRITE-AU-EXIT.
+           EXIT.   
 
        P2. 
            MOVE SPACE TO FILEOUT01
-           STRING "SOV employer or BCBSVT member " CD-PROC1 " " 
-             CD-DATE-T " " CD-KEY8 " needs PA" 
+           STRING "BCBSVT AUTH " AUTH-NUM " ADDED FOR " CD-KEY8 " ON " 
+             CD-DATE-T " FOR THE " CD-PROC1
              DELIMITED BY SIZE INTO FILEOUT01
-           WRITE FILEOUT01 
-           GO TO P1.
+           WRITE FILEOUT01.
 
        P3. 
            MOVE SPACE TO FILEOUT01
-           STRING "BCBSVT member only            " CD-PROC1 " " 
-             CD-DATE-T " " CD-KEY8 " needs PA" 
+           STRING CHARFILE-KEY " NO BCBSVT AUTH ON " 
+             CD-DATE-T " FOR THE " CD-PROC1
              DELIMITED BY SIZE INTO FILEOUT01
-           WRITE FILEOUT01 
-           GO TO P1.
-    
+           WRITE FILEOUT01.
+
+       P4. 
+           MOVE SPACE TO FILEOUT01
+           STRING CHARFILE-KEY " DATE MATCHES BUT NOT A GOOD AUTH, " 
+               HOLD-AUTH ", ON " CD-DATE-T " FOR THE " CD-PROC1
+             DELIMITED BY SIZE INTO FILEOUT01
+           WRITE FILEOUT01.    
 
        P99.
-           CLOSE CHARFILE FILEOUT. 
+           CLOSE CHARFILE GARFILE EMAILAUTHFILE AUTHFILE FILEOUT. 
            STOP RUN.
+
