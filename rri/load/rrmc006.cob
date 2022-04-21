d81dd7a1ce383e09619e0cf5611945f023ceafdb
stephen waite <stephen.waite@cmsvt.com>
2022-04-21 10:24:08 -0400

    typo

--- rri/load/rrmc006.cob
+++ rri/load/rrmc006.cob
@@ -1003,7 +1003,7 @@
            IF R1-IP1 = "00930"
                PERFORM REPLACE-1 THRU REPLACE-1-EXIT.
            
-           IF R1-IP1 = "00433" OR "00698" OR "00699" OR  OR "00830"
+           IF R1-IP1 = "00433" OR "00698" OR "00699" OR "00830"
                PERFORM REPLACE-2 THRU REPLACE-2-EXIT
            END-IF
 
