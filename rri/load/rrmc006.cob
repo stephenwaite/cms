abb57101cc21777d942833e6b2aab796af0bba0a
stephen waite <stephen.waite@cmsvt.com>
2022-06-05 13:34:29 -0400

    move acc in rri-248

--- rri/load/rrmc006.cob
+++ rri/load/rrmc006.cob
@@ -1713,6 +1713,7 @@
            
            MOVE R3-CLINICAL TO C-CLINICAL
            MOVE R2-DIAG TO C-ADMIT-DIAG
+           MOVE R3-ACCESSION TO C-ACCESSION
            MOVE R3-CPT TO C-CPT
            MOVE R3-MOD1 TO C-MOD2
            MOVE R3-MOD2 TO C-MOD3
