7cd004069926de1c28a8cca4194a2b890740cc29
stephen waite <stephen.waite@cmsvt.com>
2022-06-05 14:57:35 -0400

    252 fixes

--- rri/load/rri252.cob
+++ rri/load/rri252.cob
@@ -11,8 +11,8 @@
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
 
-           SELECT CHARFILE ASSIGN TO "S30" ORGANIZATION IS INDEXED
-           ACCESS MODE IS DYNAMIC RECORD KEY IS CHARFILE-KEY.
+           SELECT CHARNEW ASSIGN TO "S30" ORGANIZATION IS INDEXED
+           ACCESS MODE IS DYNAMIC RECORD KEY IS    CHARNEW-KEY.
 
            SELECT FILEOUT ASSIGN TO "S35" ORGANIZATION
            LINE SEQUENTIAL.
@@ -21,52 +21,8 @@
 
        FILE SECTION.
 
-       FD  CHARFILE
-           DATA RECORD IS CHARFILE01.
-       01  CHARFILE01.
-           02 CHARFILE-KEY.
-             03 CD-KEY8 PIC X(8).
-             03 CD-KEY3 PIC XXX.
-           02 CD-PATID PIC X(8).
-           02 CD-CLAIM PIC X(6).
-           02 CD-SERVICE PIC X.
-           02 CD-DIAG PIC X(7).
-           02 CD-PROC1 PIC X(4).
-           02 CD-PROC2 PIC X(7).
-           02 CD-MOD2 PIC XX.
-           02 CD-MOD3 PIC XX.
-           02 CD-MOD4 PIC XX.
-           02 CD-AMOUNT PIC S9(4)V99.
-           02 CD-DOCR PIC X(3).
-           02 CD-DOCP PIC X(2).
-           02 CD-PAYCODE PIC XXX.
-           02 CD-STAT PIC X.
-           02 CD-WORK PIC XX.
-           02 CD-DAT1 PIC X(8).
-           02 CD-RESULT PIC X.
-           02 CD-ACT PIC X.
-           02 CD-SORCREF PIC X.
-           02 CD-COLLT PIC X.
-           02 CD-AGE PIC X.
-           02 CD-PAPER PIC X.
-           02 CD-PLACE PIC X.
-           02 CD-NAME PIC X(24).
-           02 CD-ESPDT PIC X.
-           02 CD-DATE-T PIC X(8).
-           02 CD-DATE-E PIC X(8).
-           02 CD-ORDER PIC X(6).
-           02 CD-DX2 PIC X(7).
-           02 CD-DX3 PIC X(7).
-           02 CD-DATE-A PIC X(8).
-           02 CD-ACC-TYPE PIC X.
-           02 CD-DATE-M PIC X(8).
-           02 CD-ASSIGN PIC X.
-           02 CD-NEIC-ASSIGN PIC X.
-           02 CD-DX4 PIC X(7).
-           02 CD-DX5 PIC X(7).
-           02 CD-DX6 PIC X(7).
-           02 CD-CLINICAL PIC X(40).
-           02 CD-ADMIT-DIAG PIC X(30).
+       FD  CHARNEW.
+           copy "charnew.cpy" in "c:\Users\sid\cms\copylib\rri".
 
        FD  FILEOUT.
        01  FILEOUT01 PIC X(80).   
@@ -74,17 +30,17 @@
        PROCEDURE DIVISION.
 
        P0. 
-           OPEN I-O CHARFILE
+           OPEN I-O CHARNEW
            OPEN OUTPUT FILEOUT
-           MOVE SPACE TO CHARFILE-KEY
-           START CHARFILE KEY NOT < CHARFILE-KEY
+           MOVE SPACE TO CHARNEW-KEY
+           START CHARNEW KEY NOT < CHARNEW-KEY
              INVALID
                DISPLAY "EMPTY FILE"
                GO TO P2
            END-START.    
 
        P1. 
-           READ CHARFILE NEXT
+           READ CHARNEW NEXT
              AT END
                GO TO P2
            END-READ    
@@ -92,7 +48,7 @@
            IF (CD-PROC1 = "4094" OR "4095" OR "4073")
                MOVE "024" TO CD-PAYCODE
                MOVE "P" TO CD-PAPER
-               REWRITE CHARFILE01
+               REWRITE CHARNEW01
                STRING "Cardio kid? " CD-NAME " " CD-DATE-T " " CD-PROC1
                DELIMITED BY SIZE INTO FILEOUT01
                WRITE FILEOUT01
@@ -103,7 +59,7 @@
                (CD-PROC1 = "1091" OR "1092" OR "1441"))
                MOVE CD-MOD2 TO CD-MOD3
                MOVE "GG" TO CD-MOD2
-               REWRITE CHARFILE01
+               REWRITE CHARNEW01
                STRING "GG mod for same day screen with diag "
                        CD-NAME " " CD-DATE-T " " CD-PROC1
                DELIMITED BY SIZE INTO FILEOUT01
@@ -112,5 +68,5 @@
 
            GO TO P1.
        P2. 
-           CLOSE CHARFILE FILEOUT.
+           CLOSE CHARNEW FILEOUT.
            STOP RUN.
