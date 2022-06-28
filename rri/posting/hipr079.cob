ba02c8c5f369928ec593b9224874741cd8af4486
stephen waite <stephen.waite@cmsvt.com>
2022-06-28 09:08:44 -0400

    only 4 clm stat

--- rri/posting/hipr079.cob
+++ rri/posting/hipr079.cob
@@ -742,8 +742,7 @@
 
       * RECORD ARE GOOD! START MAKING PAYMENT RECORDS.
        P4-SVC-LOOP.                      
-           IF NOT (CLP-2CLMSTAT = "1 " OR "2 " OR "3 " OR "19"
-                               OR "20" OR "21")
+           IF CLP-2CLMSTAT = "4 "
       *         PERFORM P1-DENIED-SVC THRU P1-LOST-SVC
       *             VARYING X FROM 1 BY 1 UNTIL X > SVC-CNTR
       *         GO TO P9-SVC-LOOP
