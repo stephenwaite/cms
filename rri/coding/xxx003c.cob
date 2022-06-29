6c2a2ba8f985b33bc617dbdc278e3a7164e59827
stephen waite <stephen.waite@cmsvt.com>
2022-06-29 08:44:31 -0400

    try take backs for va (#221)
    * try take backs for va
    
    * remove unused
    
    * remove lost svc
    
    * debug
    
    * debug
    
    * remove dump50
    
    * debug
    
    * debugging
    
    * tot claim
    
    * tot claim
    
    * adj 15
    
    * 079 Paycode
    
    * pd
    
    * fix sign
    
    * guessing at math

--- rri/coding/xxx003c.cob
+++ rri/coding/xxx003c.cob
@@ -271,8 +271,8 @@
                display " "
                MOVE "R928   " TO CD-DIAG
       * medicare and adv plans don't follow vt call back policy         
-               IF (CD-PAYCODE = "003" OR "028" OR "074" OR "200" 
-                 OR "245" OR "270" or "697" OR "868" OR "912")
+               IF (CD-PAYCODE = "003" OR "028" OR "074" OR "141" 
+                 OR "200" OR "245" OR "270" or "697" OR "868" OR "912")
                    IF (CD-PROC1 = "1446")
                        MOVE "LT" TO CD-MOD2
                        MOVE "10937706526" TO CD-PROC
