499d1f6f63d89fb9d35e191319269f3b72e27ce4
stephen waite <stephen.waite@cmsvt.com>
2022-06-29 08:45:43 -0400

    fix to int charges and chcrr revins (#217)

--- php/int_charges/faced.php
+++ php/int_charges/faced.php
@@ -4,25 +4,36 @@ require __DIR__ . '/../../vendor/autoload.php';
 
 use League\Csv\Reader;
 
-$reader = Reader::createFromPath('/tmp/test.csv', 'r');
+$reader = Reader::createFromPath($argv[1], 'r');
 $reader->setHeaderOffset(0);
+$header = $reader->getHeader();
+//var_dump($header);
+$i = 1;
+foreach($header as $key => $value) {
+    if (empty($value)) {
+        $value = (string) $i++;
+    }
+    $new_header[$key] = $value;
+}
 
-$records = $reader->getRecords();
+//var_dump($new_header);
+
+$records = $reader->getRecords($new_header);
 foreach ($records as $offset => $record) {
     //$offset : represents the record offset
     //var_export($record); // returns something like
-    //var_dump($record);
+    //var_dump($offset);
+    //var_export($record);
+    //exit;
     //if (in_array($record['Date'], $record)) {
     //echo $record['Date'] . "\n";
-    //echo implode('', array_reverse(explode('/', $record['Date']))) . "\n";    
+    //echo implode('', array_reverse(explode('/', $record['Date']))) . "\n";
     $cdm = $record['CDM'];
     $cpt = $record['CPT'] ?: $record['HCPCS'];
     $dos = $record['DOS'];
     $mrn = $record['MRN'];
     if ($record['HCPCS'] == "G1004") {
-      $cpt = "G1004";
+        $cpt = "G1004";
     }
     echo $cdm . "," . $cpt . "," . $dos . "," . $mrn . "\n";
 }
-
-
