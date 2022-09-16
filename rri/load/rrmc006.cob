ffec088e1969154f5992ecef16b980cfcc6dd317
stephen waite <stephen.waite@cmsvt.com>
2022-09-16 13:40:48 -0400

    new ed doc

--- rri/load/rrmc006.cob
+++ rri/load/rrmc006.cob
@@ -1712,7 +1712,8 @@
            end-if
 
            
-           IF ((REF = "B1T" OR "B51" OR "B7C" 
+           IF ((REF = "A3Z"
+               OR "B1T" OR "B51" OR "B7C" 
                OR "D55" OR "D3Z"
                OR "F4J" OR "F34" OR "F2S"
                OR "G0T" OR "G0A" OR "G36" OR "G4U"
