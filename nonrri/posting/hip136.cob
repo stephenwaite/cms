c83d06757c0cbb42f556ae206518d828560aba07
stephen waite <stephen.waite@cmsvt.com>
2022-10-05 14:54:55 -0400

    debug B10 non-rri

--- nonrri/posting/hip136.cob
+++ nonrri/posting/hip136.cob
@@ -823,9 +823,7 @@
                UNSTRING FILEIN01 DELIMITED BY "*" INTO
                  CAS-0 CAS-1 CAS-2 CAS-3 CAS-4 CAS-5 CAS-6 CAS-7 
                  CAS-8 CAS-9 CAS-10 CAS-11 CAS-12 CAS-13 CAS-14 
-                 CAS-15 CAS-16 CAS-17 CAS-18 CAS-19 
-               IF (CAS-2 = "B10") MOVE CAS-3 TO ALF8
-               END-IF              
+                 CAS-15 CAS-16 CAS-17 CAS-18 CAS-19            
                
                IF ALF8 NOT = SPACE
                  MOVE "14" TO PD-DENIAL
