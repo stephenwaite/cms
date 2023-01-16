3320379a1877b42e10faa9b17900ccf755768c55
stephen waite <stephen.waite@cmsvt.com>
2023-01-16 12:51:01 -0500

    debug

--- rri/posting/hipr178.cob
+++ rri/posting/hipr178.cob
@@ -920,8 +920,6 @@
                IF CAS-SVC(Z) = X
                    MOVE SPACE TO CAS01 ALF8
                    MOVE CAS-TAB(Z) TO FILEIN01
-                   display filein01
-                   accept omitted
                    UNSTRING FILEIN01 DELIMITED BY "*" INTO
                      CAS-0 CAS-1 CAS-2 CAS-3 CAS-4 CAS-5 CAS-6 CAS-7 
                      CAS-8 CAS-9 CAS-10 CAS-11 CAS-12 CAS-13 CAS-14 
@@ -966,7 +964,9 @@
                      CAS-0 CAS-1 CAS-2 CAS-3 CAS-4 CAS-5 CAS-6 CAS-7 
                      CAS-8 CAS-9 CAS-10 CAS-11 CAS-12 CAS-13 CAS-14 
                      CAS-15 CAS-16 CAS-17 CAS-18 CAS-19 
-                   
+                   display CAS-1 " CAS-1 " CAS-2 " CAS-2" 
+                     CAS-3 " CAS-3 " CAS-4 " CAS-4 " CAS-5 " CAS-5"
+                   accept omitted
                    IF (CAS-1 = "CO" OR "PI" OR "OA")
                                AND
                       ((CAS-2 = "A2" OR "B6" OR "B10" OR
