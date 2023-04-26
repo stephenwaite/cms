6bc562c9617bb8329baf9b6792bad9208bf3420a
stephen waite <stephen.waite@cmsvt.com>
2023-04-26 08:05:02 -0400

    handle C8912 HCPCS on load

--- rri/load/rrmc008.cob
+++ rri/load/rrmc008.cob
@@ -373,7 +373,7 @@
            END-IF
 
            IF (R3-PROC = "6263" OR "6267") 
-              AND (r3-cpt = "C8912")
+              AND (r3-cpt = "C8912" OR r3-hcpcs = "C8912")
                move space to ERRFILE01
                string "RRMC SENT US " R3-CPT " " R3-HCPCS
                  " CHANGING THIS TO CPT 73725" 
