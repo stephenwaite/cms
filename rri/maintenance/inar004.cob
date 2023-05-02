4763b98699065be7ed7bead8813247621da586b7
stephen waite <stephen.waite@cmsvt.com>
2023-05-02 10:35:42 -0400

    debug

--- rri/maintenance/inar004.cob
+++ rri/maintenance/inar004.cob
@@ -3711,8 +3711,8 @@
            IF IN-FIELD = "?"
            DISPLAY "TYPE THE 2ND MODIFIER OR <CR>"
            GO TO CC-2000TI.
-           DISPLAY "IN-FIELD(1:1) " IN-FIELD(1:1)
-           DISPLAY "IN-FIELD(2:1) " IN-FIELD(2:1)
+      *     DISPLAY "IN-FIELD(1:1) " IN-FIELD(1:1)
+      *     DISPLAY "IN-FIELD(2:1) " IN-FIELD(2:1)
            IF IN-FIELD(1:1) NOT = SPACE AND IN-FIELD(2:1) = SPACE
              DISPLAY "INVALID, MOD SHOULD BE 2 CHARS"
              GO TO CC-2000TI.
