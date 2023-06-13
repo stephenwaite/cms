9237c64535b2906d128bdc0945d422e784d0ff82
stephen waite <stephen.waite@cmsvt.com>
2023-06-13 13:27:07 -0400

    fix rarc language

--- rri/posting/hipr178.cob
+++ rri/posting/hipr178.cob
@@ -1447,7 +1447,7 @@
                  READ rarcfile with lock
                    invalid
                      MOVE SPACE TO RARC-REASON
-                     STRING LQ-2 ' INVALID RARC CODE" DELIMITED BY size 
+                     STRING LQ-2 " INVALID RARC CODE" DELIMITED BY size 
                        INTO ERROR-FILE01
                      continue
                  end-read
