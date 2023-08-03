ac632ec214404210fb6fc39d4530770ece595c4f
stephen waite <stephen.waite@cmsvt.com>
2023-08-03 17:05:21 -0400

    add check for ins

--- php/rri/visitno-load.php
+++ php/rri/visitno-load.php
@@ -9,6 +9,9 @@ foreach($lines as $line) {
         $visitno = substr($line, 2, 7);
         //echo "$line\n";
         //echo "visitno $visitno \n";
+        $rrmc_ins_code = trim(substr($line, 295, 5));
+        //echo "rrmc ins code $rrmc_ins_code\n";
+        //exit;
     }
 
     if (substr($line, 0, 2) == '++') {
@@ -16,11 +19,15 @@ foreach($lines as $line) {
         //echo "key is $mrn\n";
         if (array_key_exists($mrn, $load)) {
             //echo "$mrn already in load \n";
-            if ($load[$mrn] != $visitno) {
+            if ($load[$mrn]['visitno'] != $visitno) {
                 echo "*** that's odd, visitno $visitno is different from prior record in this file for mrn $mrn\n";
+                if ($load[$mrn]['rrmc_ins_code'] != $rrmc_ins_code) {
+                    echo "*** uh oh, rrmc ins code $rrmc_ins_code is different from prior record ins code " . $load[$mrn]['rrmc_ins_code'] . " in this file for mrn $mrn\n";
+                }
             }
         }
-        $load[$mrn] = $visitno;
+        $load[$mrn]['visitno'] = $visitno;
+        $load[$mrn]['rrmc_ins_code'] = $rrmc_ins_code;
         
     }
     
