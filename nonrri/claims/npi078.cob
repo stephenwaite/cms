b626c88759716811a26b02aa26ba240247be7b93
stephen waite <stephen.waite@cmsvt.com>
2024-02-28 19:21:26 -0500

    add authfile to npi078

--- nonrri/claims/npi078.cob
+++ nonrri/claims/npi078.cob
@@ -235,16 +235,8 @@
            02 CC-DX6 PIC X(7).
            02 CC-FUTURE PIC X(6).
 
-       FD  AUTHFILE
-           DATA RECORD IS AUTHFILE01.
-       01  AUTHFILE01.
-           02 AUTH-KEY.
-              03 AUTH-KEY8 PIC X(8).
-              03 AUTH-KEY6 PIC X(6).
-           02 AUTH-NUM PIC X(15).
-           02 AUTH-QNTY PIC XX.
-           02 AUTH-DATE-E PIC X(8).
-           02 AUTH-FILLER PIC X(41).           
+       FD  AUTHFILE.
+           COPY authfile.CPY IN "C:\Users\sid\cms\copylib".        
  
        WORKING-STORAGE SECTION.
        01  INSTAB01.
