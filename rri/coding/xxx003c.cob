fe27dc7e0215828970d9fabc4124a39cafa5d867
stephen waite <stephen.waite@cmsvt.com>
2024-03-08 11:26:47 -0500

    add champva to 77065 switch

--- rri/coding/xxx003c.cob
+++ rri/coding/xxx003c.cob
@@ -284,7 +284,8 @@
                MOVE "R928   " TO CD-DIAG
       * medicare and adv plans don't follow vt call back policy         
                IF (CD-PAYCODE = "003" OR "028" OR "074" OR "141" 
-                 OR "200" OR "245" OR "270" or "697" OR "868" OR "912")
+                 OR "200" OR "245" OR "270" or "697" OR "868" OR "912"
+                 OR "409")
                    IF (CD-PROC1 = "1446")
                        MOVE "LT" TO CD-MOD2
                        MOVE "10937706526" TO CD-PROC
