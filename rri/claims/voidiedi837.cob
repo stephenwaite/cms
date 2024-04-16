e0b9ff92b0b033da85ca736aa3ee6e20ed426bb7
stephen waite <stephen.waite@cmsvt.com>
2024-04-16 12:25:16 -0400

    fix if cond

--- rri/claims/voidiedi837.cob
+++ rri/claims/voidiedi837.cob
@@ -1012,24 +1012,22 @@
              MOVE HOLD-FILEIN-KEY TO PWK-7
              WRITE SEGFILE01 FROM PWK01.
              
-      *    add auth for VA/VACCN outpatient claims
+      *    add auth 
            MOVE 0 TO AUTH-FLAG
-      *     IF (HOLD-PAYCODE = "079" OR "225" OR "926")
-              MOVE HOLD-KEY8 TO AUTH-KEY8
-              MOVE HOLD-CLAIM TO AUTH-KEY6
-              READ AUTHFILE INVALID
-                  MOVE 1 TO AUTH-FLAG
-                  GO TO 2300CLM-EXIT
-              END-READ    
-              MOVE SPACE TO REF-CODE
-              MOVE "G1" TO REF-CODE
-              MOVE SPACE TO REF-ID
-              IF (AUTH-FLAG = 0 AND AUTH-NUM NOT = SPACE)
-                MOVE AUTH-NUM TO REF-ID
-      *        ELSE
-      *          MOVE "VA9999999999" TO REF-ID
-              END-IF  
-      *     END-IF.  
+           MOVE HOLD-KEY8 TO AUTH-KEY8
+           MOVE HOLD-CLAIM TO AUTH-KEY6
+           READ AUTHFILE INVALID
+               MOVE 1 TO AUTH-FLAG
+               GO TO 2300CLM-EXIT
+           END-READ    
+           MOVE SPACE TO REF-CODE
+           MOVE "G1" TO REF-CODE
+           MOVE SPACE TO REF-ID
+           IF (AUTH-FLAG = 0 AND AUTH-NUM NOT = SPACE)
+               MOVE AUTH-NUM TO REF-ID
+               MOVE SPACE TO SEGFILE01
+               WRITE SEGFILE01 FROM REF01
+           END-IF
 
       *    claim number for corrected/voided claims
            MOVE SPACE TO REF-CODE
