338d2ad6018fa16ba5f22b0e855fc2c8298035cf
stephen waite <stephen.waite@cmsvt.com>
2025-02-27 15:44:05 -0500

    fixed and creating new script

--- bcbsvt/check_x12s.php
+++ bcbsvt/check_x12s.php
@@ -4,6 +4,7 @@ use phpseclib3\Net\SFTP;
 
 require_once(dirname(__FILE__) . '/../vendor/autoload.php');
 
+
 $cms_user = getenv('MOVEIT_USERNAME');
 $cms_pass = getenv('MOVEIT_PASSWORD');
 $sftp = new SFTP('moveit.bcbsvt.com');
@@ -12,7 +13,7 @@ if (!$sftp->login($cms_user, $cms_pass)) {
     exit;
 };
 
-$path = '/Home/cms/';
+$path = '/Home/cms';
 
 $rawlist = $sftp->rawlist($path, true);
 if (!empty($rawlist)) {
@@ -20,29 +21,10 @@ if (!empty($rawlist)) {
         if (!empty($file)) {
             $dt_utc = new DateTimeImmutable(date('Y-m-d h:i:s a', $file->mtime));
             $date = $dt_utc->setTimezone(new DateTimeZone('America/New_York'));
-            $fileName = $file->filename;
-            echo "file: " . $fileName . " uploaded to 02 on " .
+            echo "file: " . $file->filename . " uploaded to 02 on " .
                 $date->format('Y-m-d h:i:s a') . "\n";
-            if (stripos($fileName, 'txt') !== false) {
-                echo "we have to change this filename to 00711##.x12 \n";
-                changeFileName($fileName, $sftp);
-            }
         }
     }
 } else {
     // there's a test directory
-}
-
-function changeFileName($fileName, $sftp)
-{
-    try {
-        $moveitFilename = "007111" . rand(10, 99) . ".x12";
-        echo "file name $moveitFilename doesn't exist so we can rename it \n";
-        if (!$sftp->rename($fileName, $moveitFilename)) {
-            throw new Exception('rename failed');
-        }
-        echo "changed name successfully \n";
-    } catch (Exception $e) {
-        echo "changed name unsuccessfully " . $e->getMessage();
-    }
 }
