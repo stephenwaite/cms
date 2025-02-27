2c2bcaffa9cfc9bf0f1c9174eaa975daa4ad9905
stephen waite <stephen.waite@cmsvt.com>
2025-02-27 14:01:23 -0500

    debug rename

--- bcbsvt/check_x12s.php
+++ bcbsvt/check_x12s.php
@@ -21,8 +21,12 @@ if (!empty($rawlist)) {
         if (!empty($file)) {
             $dt_utc = new DateTimeImmutable(date('Y-m-d h:i:s a', $file->mtime));
             $date = $dt_utc->setTimezone(new DateTimeZone('America/New_York'));
-            echo "file: " . $file->filename . " uploaded to 02 on " .
+            $filename = $file->filename;
+            echo "file: " . $filename . " uploaded to 02 on " .
                 $date->format('Y-m-d h:i:s a') . "\n";
+            if (stripos($file->filename, 'txt') !== false) {
+                echo "we have to change this filename to 00711##.x12 \n";
+            }
         }
     }
 } else {
