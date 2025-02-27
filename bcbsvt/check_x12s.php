cff4dc146a85b1ba8f9144709a050c732dc70149
stephen waite <stephen.waite@cmsvt.com>
2025-02-27 15:14:34 -0500

    use sftp rename method and path

--- bcbsvt/check_x12s.php
+++ bcbsvt/check_x12s.php
@@ -4,7 +4,6 @@ use phpseclib3\Net\SFTP;
 
 require_once(dirname(__FILE__) . '/../vendor/autoload.php');
 
-
 $cms_user = getenv('MOVEIT_USERNAME');
 $cms_pass = getenv('MOVEIT_PASSWORD');
 $sftp = new SFTP('moveit.bcbsvt.com');
@@ -13,7 +12,7 @@ if (!$sftp->login($cms_user, $cms_pass)) {
     exit;
 };
 
-$path = '/Home/cms';
+$path = '/Home/cms/';
 
 $rawlist = $sftp->rawlist($path, true);
 if (!empty($rawlist)) {
@@ -21,13 +20,12 @@ if (!empty($rawlist)) {
         if (!empty($file)) {
             $dt_utc = new DateTimeImmutable(date('Y-m-d h:i:s a', $file->mtime));
             $date = $dt_utc->setTimezone(new DateTimeZone('America/New_York'));
-            // sftp needs to have the full dir
-            $fileName = "/Home/cms/" . $file->filename;
+            $fileName = $file->filename;
             echo "file: " . $fileName . " uploaded to 02 on " .
                 $date->format('Y-m-d h:i:s a') . "\n";
             if (stripos($fileName, 'txt') !== false) {
                 echo "we have to change this filename to 00711##.x12 \n";
-                changeFileName($fileName);
+                changeFileName($path, $fileName, $sftp);
             }
         }
     }
@@ -35,19 +33,19 @@ if (!empty($rawlist)) {
     // there's a test directory
 }
 
-function changeFileName($fileName)
+function changeFileName($path, $fileName, $sftp)
 {
-    $moveitFilename = "/Home/cms/" . "007111" . rand(10, 99) . ".x12";
+    $moveitFilename = $path . "007111" . rand(10, 99) . ".x12";
     if (!checkFileNameExists($moveitFilename)) {
         echo "file name $moveitFilename doesn't exist so we can rename it \n";
-        if (rename($fileName, $moveitFilename)) {
+        if ($sftp->rename($path . $fileName, $path . $moveitFilename)) {
             echo "changed name successfully \n";
         } else {
             echo "changed name unsuccessfully \n";
         }
     } else {
         echo "file name does exists so we have to try changing the file name";
-        //changeFileName($fileName);
+        changeFileName($path, $fileName, $sftp);
     }
 }
 
