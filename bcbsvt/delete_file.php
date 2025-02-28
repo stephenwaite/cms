0c6b53b7e5a894429173a456b80171a771576a69
stephen waite <stephen.waite@cmsvt.com>
2025-02-28 17:46:56 -0500

    fix delete_file

--- bcbsvt/delete_file.php
+++ bcbsvt/delete_file.php
@@ -4,11 +4,17 @@ use phpseclib3\Net\SFTP;
 
 require_once(dirname(__FILE__) . '/../vendor/autoload.php');
 
-
 $cms_user = getenv('MOVEIT_USERNAME');
 $cms_pass = getenv('MOVEIT_PASSWORD');
 $sftp = new SFTP('moveit.bcbsvt.com');
-$sftp->login($cms_user, $cms_pass);
+
+try {
+    $sftp->login($cms_user, $cms_pass);
+    echo "log in succeeded \n";
+} catch (Exception $e) {
+    throw new Exception("login failed");
+    die;
+}
 
 $remote_path = "/Home/cms/";
 $line = readline("really want to delete " . $argv[1] . "? enter Y or y then");
