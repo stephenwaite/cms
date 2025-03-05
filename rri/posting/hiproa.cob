e96fe2ae75db4d4459f92317b05f7ba82765f440
stephen waite <stephen.waite@cmsvt.com>
2025-03-05 09:45:25 -0500

    debug

--- rri/posting/hiproa.cob
+++ rri/posting/hiproa.cob
@@ -551,6 +551,9 @@
                    WRITE REMITFILE01
                    END-WRITE
                NOT INVALID
+                   DISPLAY "TRN-0 " TRN-0 " TRN-1" TRN-1 " TRN-2 " TRN-2
+                    " TRN-3 " TRN-3 " TRN-4 " TRN-4
+                   ACCEPT OMITTED
                    IF TRN-3 NOT = "0000000000"
                        MOVE SPACE TO ERROR-FILE01
                        STRING REMITFILE01 " DUPE CHECK"
