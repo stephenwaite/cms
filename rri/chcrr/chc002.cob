b7e9a6ec1db804bab5bf20f000e2187713313a7e
stephen waite <stephen.waite@cmsvt.com>
2025-10-03 14:28:32 -0400

    fix for zip4

--- rri/chcrr/chc002.cob
+++ rri/chcrr/chc002.cob
@@ -72,6 +72,7 @@
        WORKING-STORAGE SECTION.
        01  TAB-X PIC X VALUE H"09".
        01  ALF9 PIC X(9).
+       01  TEMP-ZIP PIC X(9).
 
        PROCEDURE DIVISION.
        0005-S0TART.
@@ -136,6 +137,11 @@
       *     ACCEPT OMITTED
 
            MOVE SPACE TO ALF9
+           IF FO-PAT-ZIP(6:4) = "0000"
+               MOVE SPACE TO TEMP-ZIP
+               MOVE FO-PAT-ZIP(1:5) TO TEMP-ZIP
+               MOVE TEMP-ZIP TO FO-PAT-ZIP    
+           END-IF
            STRING FO-PAT-ZIP(1:5) FO-PAT-ZIP(7:4) INTO ALF9
            MOVE SPACE TO FO-PAT-ZIP
            MOVE ALF9 TO FO-PAT-ZIP
