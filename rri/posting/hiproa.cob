1e4aaddef76280e437f6507b0fa599e212230e99
stephen waite <stephen.waite@cmsvt.com>
2026-03-22 18:16:20 -0400

    remove cas code check on 2ndaries

--- rri/posting/hiproa.cob
+++ rri/posting/hiproa.cob
@@ -828,25 +828,6 @@
 
            IF PD-AMOUNT = 0
                IF CLP-2CLMSTAT = "2 "
-                   PERFORM VARYING Z FROM 1 BY 1 UNTIL Z > CAS-CNTR
-                       IF CAS-SVC(Z) = X
-                           MOVE SPACE TO CAS01
-                           MOVE CAS-TAB(Z) TO FILEIN01
-                           UNSTRING FILEIN01 DELIMITED BY "*" INTO
-                               CAS-0 CAS-1 CAS-2 CAS-3 CAS-4 CAS-5
-                               CAS-6 CAS-7 CAS-8 CAS-9 CAS-10 CAS-11
-                               CAS-12 CAS-13 CAS-14 CAS-15 CAS-16
-                               CAS-17 CAS-18 CAS-19
-                           MOVE CAS-2 TO CAS-CODE-CHECK
-                           IF NOT INS-REDUCE-CODE
-                               MOVE CAS-5 TO CAS-CODE-CHECK
-                           END-IF
-                           IF NOT INS-REDUCE-CODE
-                               PERFORM P1-LOST-SVC
-                               GO TO P5-SVC-LOOP-EXIT
-                           END-IF
-                       END-IF
-                   END-PERFORM
                    GO TO P5-SVC-LOOP-EXIT
                END-IF
                MOVE 0 TO FLAG
