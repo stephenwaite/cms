2823c05c2a5710bcab90b06fcc4137b225ccf8e5
stephen waite <stephen.waite@cmsvt.com>
2026-05-08 16:34:49 -0400

    Move dd from p7-next to p5-svc-loop

--- rri/posting/hiproa.cob
+++ rri/posting/hiproa.cob
@@ -878,6 +878,48 @@
                END-IF
            END-IF
 
+           MOVE "  " TO PD-DENIAL.
+
+           PERFORM VARYING Z FROM 1 BY 1 UNTIL Z > CAS-CNTR
+               IF CAS-SVC(Z) = X
+                   MOVE SPACE TO CAS01
+                   MOVE CAS-TAB(Z) TO FILEIN01
+                   UNSTRING FILEIN01 DELIMITED BY "*" INTO
+                       CAS-0 CAS-1 CAS-2 CAS-3 CAS-4 CAS-5 CAS-6 CAS-7
+                       CAS-8 CAS-9 CAS-10 CAS-11 CAS-12 CAS-13 CAS-14
+                       CAS-15 CAS-16 CAS-17 CAS-18 CAS-19
+
+                   IF (CAS-2 = "1  " OR CAS-2 = "126"
+                       OR CAS-2 = "25 " OR CAS-2 = "37 ")
+                   OR (CAS-5 = "1  " OR CAS-5 = "126"
+                       OR CAS-5 = "25 " OR CAS-5 = "37 ")
+                   OR (CAS-8 = "1  " OR CAS-8 = "126"
+                       OR CAS-8 = "25 " OR CAS-8 = "37 ")
+                   OR (CAS-11 = "1  " OR CAS-11 = "126"
+                       OR CAS-11 = "25 " OR CAS-11 = "37 ")
+                   OR (CAS-14 = "1  " OR CAS-14 = "126"
+                       OR CAS-14 = "25 " OR CAS-14 = "37 ")
+                   OR (CAS-17 = "1  " OR CAS-17 = "126"
+                       OR CAS-17 = "25 " OR CAS-17 = "37 ")
+                   MOVE "DD" TO PD-DENIAL
+                   MOVE CAS-CNTR TO Z
+                   END-IF
+               END-IF
+           END-PERFORM
+
+           IF PD-AMOUNT = 0 AND PD-DENIAL = "  "
+               MOVE 0 TO FLAG
+               PERFORM DUMP50
+               IF FLAG = 1
+                   PERFORM P1-LOST-SVC
+                   GO TO P5-SVC-LOOP-EXIT
+               END-IF
+               IF CLP-2CLMSTAT = "2 "
+                   PERFORM P1-LOST-SVC
+                   GO TO P5-SVC-LOOP-EXIT
+               END-IF
+           END-IF
+
            GO TO A6.
 
        A6.
@@ -912,35 +954,6 @@
            GO TO P5-SVC-LOOP-EXIT.
 
        P7-NEXT.
-           MOVE "  " TO PD-DENIAL.
-
-           PERFORM VARYING Z FROM 1 BY 1 UNTIL Z > CAS-CNTR
-               IF CAS-SVC(Z) = X
-                   MOVE SPACE TO CAS01
-                   MOVE CAS-TAB(Z) TO FILEIN01
-                   UNSTRING FILEIN01 DELIMITED BY "*" INTO
-                       CAS-0 CAS-1 CAS-2 CAS-3 CAS-4 CAS-5 CAS-6 CAS-7
-                       CAS-8 CAS-9 CAS-10 CAS-11 CAS-12 CAS-13 CAS-14
-                       CAS-15 CAS-16 CAS-17 CAS-18 CAS-19
-
-                   IF (CAS-2 = "1  " OR CAS-2 = "126"
-                       OR CAS-2 = "25 " OR CAS-2 = "37 ")
-                   OR (CAS-5 = "1  " OR CAS-5 = "126"
-                       OR CAS-5 = "25 " OR CAS-5 = "37 ")
-                   OR (CAS-8 = "1  " OR CAS-8 = "126"
-                       OR CAS-8 = "25 " OR CAS-8 = "37 ")
-                   OR (CAS-11 = "1  " OR CAS-11 = "126"
-                       OR CAS-11 = "25 " OR CAS-11 = "37 ")
-                   OR (CAS-14 = "1  " OR CAS-14 = "126"
-                       OR CAS-14 = "25 " OR CAS-14 = "37 ")
-                   OR (CAS-17 = "1  " OR CAS-17 = "126"
-                       OR CAS-17 = "25 " OR CAS-17 = "37 ")
-                   MOVE "DD" TO PD-DENIAL
-                   MOVE CAS-CNTR TO Z
-                   END-IF
-               END-IF
-           END-PERFORM
-
            IF NOT (PD-PAYCODE = G-PRINS 
                    OR PD-PAYCODE = G-SEINS 
                    OR PD-PAYCODE = G-TRINS
