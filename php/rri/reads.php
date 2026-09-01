6b0a70a32cd5d2c08c3ddb7680612c1938998998
Stephen Waite <stephen.waite@cmsvt.com>
2026-08-31 21:58:23 -0400

    Reads (#352)
    * fix: use localhost
    
    * fix: no refresh token

--- php/rri/reads.php
+++ php/rri/reads.php
@@ -309,13 +309,13 @@ $base_url = getenv('BASE_OEMR_URL');
 $site_id  = getenv('OEMR_RRI_SITE_ID');
 $base_uri = $base_url . '/oauth2/' . $site_id . '/token';
 
-$guzzle = new Client();
+$guzzle = new Client(['verify' => false]);
 
 $response = $guzzle->post($base_uri, [
     'form_params' => [
         'grant_type' => 'password',
         'client_id'  => getenv('OEMR_TEST_CLIENT_ID'),
-        'scope'      => "openid fhirUser online_access offline_access user/Observation.read user/Patient.read",
+        'scope'      => "openid fhirUser online_access user/Observation.read user/Patient.read",
         'user_role'  => 'users',
         'username'   => getenv('OEMR_RRI_USERNAME'),
         'password'   => getenv('OEMR_RRI_PASSWORD'),
@@ -324,7 +324,7 @@ $response = $guzzle->post($base_uri, [
 
 $bearer = json_decode((string) $response->getBody(), true)['access_token'];
 
-$client = new Client();
+$client = new Client(['verify' => false]);
 
 $headers = [
     'Authorization' => 'Bearer ' . $bearer,
